ca6b6de792911c7adafa2e7f4c91180d42b96fe6
Hiren bhatt <hirenbhatt2511@gmail.com>
2022-07-21 22:25:53 +0100

    gitcommited

---
+++ Gitpractice.java
@@ -0,0 +1,10 @@
+package github;
+
+public class Gitpractice {
+    public static void main(String[] args) {
+        int a = 250;
+        int b = 350;
+
+        System.out.println(a+b);
+    }
+}
